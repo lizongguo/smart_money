4766778269ec7f32f358d8a1be4fa0921b0903c5
lizongguo <lizongguo@bravesoft.co.jp>
2020-08-18 16:11:54 +0800

    del

--- resources/views/agent/common/user_menu.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="company_menu">
-    <ul>
-        @foreach($web_user_menu as $k => $v)
-            <li @if(isset($menu_active) && $k == $menu_active) class="active" @endif><a href="{{ $v['url'] }}">{{ $v['name'] }}</a></li>
-        @endforeach
-    </ul>
-</div>
